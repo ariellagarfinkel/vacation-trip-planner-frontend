c1ba214b91e7fe1688186a8223e01ec0aa79c42d
Ariella Garfinkel <ariellagarfinkel@gmail.com>
2023-09-06 16:02:18 -0500

    add hardcoded data and pass as props

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,10 +1,27 @@
 import { TripsIndex } from "./TripsIndex";
 
 export function Content() {
+  const trips = [
+    { id: 1, title: "Africa", image_url: "africa.jpg", start_time: "January 8, 2023", end_time: "February 1, 2023" },
+    {
+      id: 2,
+      title: "Australia",
+      image_url: "australia.jpg",
+      start_time: "January 8, 2023",
+      end_time: "February 1, 2023",
+    },
+    {
+      id: 3,
+      title: "Scotland",
+      image_url: "scotland.jpg",
+      start_time: "January 8, 2023",
+      end_time: "February 1, 2023",
+    },
+  ];
   return (
     <div>
       <h1>Welcome to React!</h1>
-      <TripsIndex />
+      <TripsIndex trips={trips} />
     </div>
   );
 }
