bd6b83cdf7474f6ade1f1639e3a5ad655e4ce83f
Ariella Garfinkel <ariellagarfinkel@gmail.com>
2023-09-07 14:12:40 -0500

    functional ugly looking cards

--- src/TripsIndex.jsx
+++ src/TripsIndex.jsx
@@ -1,6 +1,44 @@
 /* eslint-disable react/prop-types */
 export function TripsIndex(props) {
   return (
+    <>
+      <div>
+        <h1>My Trips</h1>
+        {props.trips.map((trip) => (
+          <div key={trip.id}>
+            <div className="card">
+              <img src={trip.image_url} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">{trip.title}</h5>
+                <p className="card-text">{trip.start_time}</p>
+                <p className="card-text">{trip.end_time}</p>
+              </div>
+              <div className="card-body">
+                <a href="#" className="card-link">
+                  Places
+                </a>
+                <a href="#" className="card-link">
+                  Add a Place
+                </a>
+                {trip.places.map((place) => (
+                  <div key={place.id}>
+                    <p className="card-text">{place.address}</p>
+                    <img src={place.image_url} />
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+{
+  /* 
+export function TripsIndex(props) {
+  return (
     <div>
       <h1>My Trips</h1>
       {props.trips.map((trip) => (
@@ -21,4 +59,5 @@ export function TripsIndex(props) {
       ))}
     </div>
   );
+} */
 }
